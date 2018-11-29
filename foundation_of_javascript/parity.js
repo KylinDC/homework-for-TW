22610118b18e5c22ef6ccd350f99bd6b633c7cc8
KylinDC <inlovingdays@gmail.com>
2018-11-29 12:48:32 +0800

    package the loop statement into a function

--- foundation_of_javascript/parity.js
+++ foundation_of_javascript/parity.js
@@ -1,12 +1,16 @@
-for (let i = 0; i <= 20; i++) {
-  if (isOdd(i)) {
-    console.log(i + '是奇数。');
-  }
-  else {
-    console.log(i + '是偶数。');
+function loopLogNumberParity(number) {
+  for (let i = 0; i <= number; i++) {
+    if (isOdd(i)) {
+      console.log(i + '是奇数。');
+    }
+    else {
+      console.log(i + '是偶数。');
+    }
   }
 }
 
 function isOdd(num) {
   return num % 2 !== 0;
 }
+
+loopLogNumberParity(20);
