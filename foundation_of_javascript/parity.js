09b0134d559900dea61bfdedd2fe17d78ae1c5ad
KylinDC <inlovingdays@gmail.com>
2018-11-29 11:00:56 +0800

    optimize the judgement of parity

--- foundation_of_javascript/parity.js
+++ foundation_of_javascript/parity.js
@@ -1,12 +1,12 @@
 for (let i = 0; i <= 20; i++) {
-  if (isAnEven(i) === true) {
-    console.log(i + '是偶数。');
+  if (isOdd(i)) {
+    console.log(i + '是奇数。');
   }
   else {
-    console.log(i + '是奇数。');
+    console.log(i + '是偶数。');
   }
 }
 
-function isAnEven(num) {
-  return num % 2 === 0 ? true : false;
+function isOdd(num) {
+  return num % 2;
 }
