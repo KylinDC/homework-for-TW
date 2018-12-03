d1264f44e7680f2c8b061450f164957bfa23f6d1
KylinDC <inlovingdays@gmail.com>
2018-12-03 15:15:18 +0800

    add the feature of sum the object value

--- foundation_of_javascript/object.js
+++ foundation_of_javascript/object.js
@@ -4,3 +4,21 @@ user.name = 'Jonh';
 user['surname'] = 'Mike';
 user.name = 'Peter';
 delete user.name;
+
+
+var fruit = {
+  apple: 20,
+  pear: 20,
+  peach: 10,
+  valueSum: function() {
+    let sum = 0;
+    for (let element in this) {
+      if (typeof this[element] === 'number') {
+        sum += this[element];
+      }
+    }
+    return sum;
+  }
+};
+
+console.log(fruit.valueSum());
